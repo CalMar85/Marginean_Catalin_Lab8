0d8863e53dd6ca654a0cfedfc8bd84a0452da054
catalin.marginean1 <catalin.marginean1@gmail.com>
2021-11-05 19:27:24 +0200

    Add project files.

---
+++ Data/Marginean_Catalin_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Marginean_Catalin_Lab8.Models;
+
+namespace Marginean_Catalin_Lab8.Data
+{
+    public class Marginean_Catalin_Lab8Context : DbContext
+    {
+        public Marginean_Catalin_Lab8Context (DbContextOptions<Marginean_Catalin_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Marginean_Catalin_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Marginean_Catalin_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Marginean_Catalin_Lab8.Models.Category> Category { get; set; }
+    }
+}
